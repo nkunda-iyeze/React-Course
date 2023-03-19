aea3857d48052f9a65eb7849cad50295b3ddd170
parlom <shalom.iyeze@gmail.com>
2023-03-19 16:51:38 +0200

    fetching data example 1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import {useState } from 'react';
+import {useEffect, useState } from 'react';
 import './App.css';
-import Text from './components/Text';
-
+import Axios from 'axios';
+ // fetch("https://catfact.ninja/fact")
+  // .then((response) => response.json())
+  //   .then((data) => console.log(data));
 function App() {
-  const [showText ,setShowText]= useState(false)
-  
+  const [catfact, setCatFact] = useState("")
+  const fetchCatData = () => {
+    Axios.get("https://catfact.ninja/fact")
+    .then((responce) => setCatFact(responce.data.fact));
+  }
+  useEffect(() => {
+    fetchCatData();
+  },[]);
   return (
     <div className='App'>
-      <button onClick={()=> setShowText(!showText)}>ShowText</button>
-       {showText && <Text/>}
+      <button onClick={fetchCatData}>Generate Quotes</button>
+      <p>{catfact}</p>
     </div>
-  )
+  );
   
-}
+};
 export default App;
